ef4b037ad7d1c6e9ca64777a631c505f1dbcf8d6
scommegna <scommegnal@hotmail.com>
2023-11-01 08:53:00 -0300

    feat: views and templates integration

--- core/views.py
+++ core/views.py
@@ -1,5 +1,14 @@
 from django.views.generic import TemplateView
 
+from .models import Servicos, Funcionario
+
 class IndexView(TemplateView):
     template_name = 'index.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context['servicos'] = Servicos.objects.order_by('?').all()
+        context['funcionarios'] = Funcionario.objects.order_by('?').all()
+        
+        return context
     
